6fe5d3cbbd42f8568ffa08a1abb10efc5bf5a183
Igor Tokarev <igor.tokarev@outlook.com>
2022-06-15 13:30:04 +0200

    add React Router 6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import logo from './images/logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Home from './views/Home/Home';
 import Login from './views/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Login/>
+      <div>
+        <Routes>
+        <Route 
+            path='/' 
+            element={<Home />} 
+          />
+        <Route 
+            path='/login' 
+            element={<Login />} 
+          />
+        </Routes>
+      </div>
       
     </div>
   );
---
+++ src/views/Home/Home.js
@@ -0,0 +1,15 @@
+import styles from './Home.module.scss'
+
+function Home() {
+
+
+  return(
+    <div>
+      <h1>
+        Home page
+      </h1>
+    </div>
+  )
+}
+
+export default Home;
