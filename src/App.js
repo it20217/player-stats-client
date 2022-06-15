ad39e5c687cca22acfc7fba07543541e21f5112b
Igor Tokarev <igor.tokarev@outlook.com>
2022-06-15 12:40:03 +0200

    create Login page view

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import logo from './images/logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Login from './views/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <h1 className="text-white text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Login/>
       
     </div>
   );
